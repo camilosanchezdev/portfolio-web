45977155e4f3ef6e53e362effd4e2e901e2b9063
Camilo Sanchez <camilosanchezdev@gmail.com>
2023-04-08 02:02:06 -0300

    feat - install eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-function App() {
+import HomePage from "./containers/HomePage"
 
+function App() {
   return (
     <div className="App">
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <HomePage />
     </div>
   )
 }
